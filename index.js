bb2587f57f4dd1ce7d2aa94c170da67b5f106475
Marissa Zhong <mwz2101@columbia.edu>
2018-07-21 12:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ function createPost() {
   let postAuthor = document.getElementById("postAuthor").value;
   let postBody = document.getElementById("postBody").value;
 
-  
+  let templateFnPage = _.template(document.getElementById("page-template").innerHTML);
+  let templateFnPost = _.template(document.getElementById("post-template").innerHTML);
+  let templateFnComments = _.template(document.getElementById("comments-template").innerHTML);
+
+  //page
+  let pageMain = document.getElementByTagName("main")[0];
+  pageMain.innerHTML += templateFnPage;
+
+  //blog
+  let post = document.getElementById("post")
+  post.innerHTML = templateFnPost({'postTitle': postTitle, 'postBody': postBody, 'postAuthor': postAuthor})
+
+  //comments
+  let comments = templateFnComments()
+  post.getElementsByTagName("footer")[0].innerHTML = comments;
 }
 
 function postComment() {
   let commenter = document.getElementById("commenterName").value;
   let comment = document.getElementById("commentText").value;
 
-  let commentTemplate = document.getElementById("comment-template").innerHTML;
-  let templateFn = _.template(commentTemplate);
+  let templateFn = _.template(document.getElementById("comment-template").innerHTML);
   let commentsDiv = document.getElementById("comments");
 
-  let templateHTML = templateFn({'comment': comment, 'commenter': commenter});
-  commentsDiv.innerHTML += templateHTML;
+  commentsDiv.innerHTML += templateFn({'comment': comment, 'commenter': commenter});
 }
